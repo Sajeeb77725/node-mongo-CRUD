b623c051389b5cc0e7d438b7a367843b84836a61
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-02-12 19:18:07 +0600

    middlewere added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+//use middlewere
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (rep, res) => {
   res.send("My node curd is running");
