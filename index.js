b6bc0a297ffb2712e368af441eec5a15f8c4e498
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-02-13 22:45:44 +0600

    mongodb connection failed forever

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,8 +8,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//dbsajeeb
+//pass: rdjnOdDnsavBHu82
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://dbsajeeb:rdjnOdDnsavBHu82@cluster1.852mvug.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("DB connection failed again");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get("/", (rep, res) => {
-  res.send("My node curd is running");
+  res.send("My node curd is running!");
 });
 
 app.listen(port, () => {
